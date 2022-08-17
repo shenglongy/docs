c5df407ad1db4999e97cc8d6cd401f0f5447b4cd
shenglongy <shenglongy@opera.com>
2022-08-17 15:14:58 +0800

    feat: add github link

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitepress'
+export default defineConfig({
+  title: "Docs",
+  themeConfig: {
+    socialLinks: [{ icon: "github", link: "https://github.com/shenglongy/docs" }]
+  },
+});
+
